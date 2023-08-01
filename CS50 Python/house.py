904b1f76fc2e01c781f17e3c29cba3bf8f7f6412
Andy-Castle <masterchief_and@live.com>
2023-08-01 15:26:43 -0500

    Aprendiendo

---
+++ CS50 Python/house.py
@@ -0,0 +1,20 @@
+name = input("Whats your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+"""
+Termine con Lecture 1 conditionals y sigue el 2
+"""
